e05f965c3f6ea023eb2d7598f84f22743db6b084
Ashish S Chettiar <ashish_chettiar@live.com>
2020-04-04 18:17:16 -0400

    initial commit
    dotnet core project for interview prep

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSACore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
