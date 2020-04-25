bbd8613990a3ec3ac91c39cfb9e832c60defd06f
Ashish S Chettiar <ashish_chettiar@live.com>
2020-04-24 22:47:20 -0400

    solution for LRU cache

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DSACore.LeetCode;
+using DSACore.DataStructures;
 
 namespace DSACore
 {
@@ -7,11 +8,23 @@ namespace DSACore
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[] { 1,2,0 };
-            var sc = new SortColorsSolution();
+            var cache = new LRUCache(3);
             Console.WriteLine("Pause to attach debugger");
             Console.Read();
-            sc.SortColors(nums);
+            cache.Put(1, 1);
+            cache.Put(2, 2);
+            cache.Put(3, 3);
+            cache.Put(4, 4);
+            Console.WriteLine(cache.Get(4));
+            Console.WriteLine(cache.Get(3));
+            Console.WriteLine(cache.Get(2));
+            Console.WriteLine(cache.Get(1));
+            cache.Put(5, 5);
+            Console.WriteLine(cache.Get(1));
+            Console.WriteLine(cache.Get(2));
+            Console.WriteLine(cache.Get(3));
+            Console.WriteLine(cache.Get(4));
+            Console.WriteLine(cache.Get(5));
             Console.WriteLine();
             Console.Read();
         }
