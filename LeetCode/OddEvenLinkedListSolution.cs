8afb2e0be8380588ef55860b219cb3e4f120624c
Ashish S Chettiar <ashish_chettiar@live.com>
2020-05-16 17:20:32 -0400

    Solution for max circular sub array

---
+++ LeetCode/OddEvenLinkedListSolution.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+
+namespace DSACore.LeetCode
+{
+    /*
+    https://leetcode.com/problems/odd-even-linked-list/
+    Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+    You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+    Example 1:
+
+    Input: 1->2->3->4->5->NULL
+    Output: 1->3->5->2->4->NULL
+    Example 2:
+
+    Input: 2->1->3->5->6->4->7->NULL
+    Output: 2->3->6->7->1->5->4->NULL
+    Note:
+
+    The relative order inside both the even and odd groups should remain as it was in the input.
+    The first node is considered odd, the second node even and so on ...
+    */
+    public class OddEvenLinkedListSolution
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode OddEvenList(ListNode head)
+        {
+            if(head == null || head.next == null || head.next.next == null) return head;
+
+            var endPointer = head;
+            var totalNodes = 0;
+            while(endPointer.next != null)
+            {
+                endPointer = endPointer.next;
+                totalNodes++;
+            }
+
+            var tempPointer = head;
+            var counter = totalNodes%2 == 0 ? totalNodes/2 : totalNodes/2 +1;
+            while(counter != 0)
+            {
+                endPointer.next = tempPointer.next;
+                tempPointer.next = tempPointer.next.next;
+                
+                tempPointer = tempPointer.next;
+                endPointer = endPointer.next;
+
+                counter--;
+            }
+
+            return head;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,19 +9,12 @@ namespace DSACore
     {
         static void Main(string[] args)
         {
-            var trie = new Trie();
+            var ca = new MaximumSumCircularSubarraySolution();
+            var input = new int[] {-3,-2,-1};
 
             Console.WriteLine("Pause to attach debugger");
             Console.Read();
-            
-            trie.Insert("hello");
-            Console.WriteLine(trie.Search("hell")); // returns false
-            Console.WriteLine(trie.Search("helloa"));     // returns false
-            Console.WriteLine(trie.Search("hello"));     // returns true
-            Console.WriteLine(trie.StartsWith("hell"));     // returns true
-            Console.WriteLine(trie.StartsWith("helloa"));     // returns false
-            Console.WriteLine(trie.StartsWith("hello"));     // returns true
-            
+            Console.WriteLine(ca.MaxSubarraySumCircular(input)); // returns false
             Console.Read();
         }
     }
