99c2a4e46248dc99d0c8ee76f4557232deb44039
Ashish S Chettiar <ashish_chettiar@live.com>
2020-06-08 13:54:06 -0400

    solution for power of two

--- Program.cs
+++ Program.cs
@@ -9,11 +9,19 @@ namespace DSACore
     {
         static void Main(string[] args)
         {
-            var ca = new CoinChange2Solution();
-            var coins = new int[] {1,2,5};
+            var ca = new QueueReconstructionByHeightSolution();
+            //[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+            var coins = new int[6][];
+            coins[0] = new int[] {7,0};
+            coins[1] = new int[] {4,4};
+            coins[2] = new int[] {7,1};
+            coins[3] = new int[] {5,0};
+            coins[4] = new int[] {6,1};
+            coins[5] = new int[] {5,2};
+
             Console.WriteLine("Pause to attach debugger");
             Console.Read();
-            Console.WriteLine(ca.Change(5, coins)); // returns false
+            Console.WriteLine(ca.ReconstructQueue(coins)); // returns false
             Console.Read();
         }
     }
