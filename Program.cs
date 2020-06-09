0ddfba8c0fb3fde18cf86e94295f395c0be36d6c
Ashish S Chettiar <ashish_chettiar@live.com>
2020-06-09 16:10:34 -0400

    solution for is subsequence

--- Program.cs
+++ Program.cs
@@ -9,19 +9,11 @@ namespace DSACore
     {
         static void Main(string[] args)
         {
-            var ca = new QueueReconstructionByHeightSolution();
-            //[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
-            var coins = new int[6][];
-            coins[0] = new int[] {7,0};
-            coins[1] = new int[] {4,4};
-            coins[2] = new int[] {7,1};
-            coins[3] = new int[] {5,0};
-            coins[4] = new int[] {6,1};
-            coins[5] = new int[] {5,2};
+            var ca = new IsSubsequenceSolution();
 
             Console.WriteLine("Pause to attach debugger");
             Console.Read();
-            Console.WriteLine(ca.ReconstructQueue(coins)); // returns false
+            Console.WriteLine(ca.IsSubsequence("","abc")); // returns true
             Console.Read();
         }
     }
